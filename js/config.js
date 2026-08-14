62047db02798e34eb47ea7b28e5d2a85ace83918
baderali203020-jpg <baderali203020@gmail.com>
2026-08-14 14:55:42 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ window.SAHHELHA_CONFIG = {
   SUPABASE_URL: 'https://wcgrgjsznzwiwhfnjyot.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_Ot8QC-w62Jmswu7T73kH9Q_zodIOK2R',
   SITE_URL: '',
-  VAPID_PUBLIC_KEY: '',
+  VAPID_PUBLIC_KEY: 'BDAl8tnYPgLdSY31Rq8tO7txV29SaAijgv99klgp8I672f9rAoNk44j5jRrR84kwWot_XEwLKcOPzJawThWcVgM',
   AI_FUNCTION_NAME: 'ai-chat',
   REMINDERS_FUNCTION_NAME: 'send-reminders',
   DELETE_ACCOUNT_FUNCTION_NAME: 'delete-account',
